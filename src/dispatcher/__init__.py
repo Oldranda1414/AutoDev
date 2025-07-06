9e84e6db2b8f790e7794cac1dcfa30a5c43a6cd4
Oldranda1414 <leonardo.randacio@gmail.com>
2025-07-06 16:13:56 +0200

    feat: implement dispatcher

--- src/dispatcher/__init__.py
+++ src/dispatcher/__init__.py
@@ -1,5 +1,13 @@
 from typing import Dict
+from handlers import generateConfig, generateDirenv
 
 def dispatch(args: Dict[str, str]):
-    print("dispatcher called")
-    print("received following args", args)
+    project_path = args["PATH-TO-PROJECT"]
+    prompt_path = args["--prompt"]
+    quiet = bool(args["--quiet"])
+    generate_direnv = args["--direnv"]
+    model_name = args["--model"]
+
+    generateConfig(project_path, model_name, prompt_path, quiet)
+    if generate_direnv:
+        generateDirenv(project_path, quiet)
---
+++ src/handlers/__init__.py
@@ -0,0 +1,5 @@
+def generateConfig(project_path: str, model: str, prompt_path: str, quiet: bool):
+    print("generate config called")
+
+def generateDirenv(project_path: str, quiet: bool):
+    print("generate direnv called")
