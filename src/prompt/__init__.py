4da4a6b4f653568c42df91d29018e93c869135be
Oldranda1414 <leonardo.randacio@gmail.com>
2025-07-13 15:34:09 +0200

    feat: add default prompt handling to prompt generation

--- src/prompt/__init__.py
+++ src/prompt/__init__.py
@@ -1,6 +1,11 @@
+import os
+
 def get_prompt(prompt_path: str, project_path: str) -> str:
-    extension = prompt_path.split(".")[-1]
-    if not extension == "json":
-        raise ValueError("prompt_path must be a json type file path") 
+    if prompt_path:
+        extension = prompt_path.split(".")[-1]
+        if not extension == "json":
+            raise ValueError("prompt_path must be a json type file path") 
+        if not os.path.exists(prompt_path):
+            raise ValueError("prompt_path provided must lead to an existing file") 
     # TODO implement this
     return "example prompt"
