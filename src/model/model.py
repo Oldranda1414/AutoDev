06be2771dad2c88ebbcdfcca69226e6540bca890
Oldranda1414 <leonardo.randacio@gmail.com>
2025-07-14 19:08:23 +0200

    refactor: replace generic error with custom error in incorrect model name implementation

--- src/model/model.py
+++ src/model/model.py
@@ -1,8 +1,10 @@
+from errors import ModelNameError
+
 class Model:
     def __init__(self, name: str, timeout: int = 5000):
         if not _is_valid_name(name):
             # TODO change this to raise ModelNameError and remove its raising in higher dependency 
-            raise ValueError(f"Model {{name}} is not one of the accepted model names")
+            raise ModelNameError(f"Model {{name}} is not one of the accepted model names")
         # TODO implement this model initialization (using litellm)
 
     def ask(self, message: str) -> str:
--- src/services/generator/generator.py
+++ src/services/generator/generator.py
@@ -3,10 +3,7 @@ from services.prompt import get_prompt
 from errors import PromptPathError, ModelNameError
 
 def generate_config(model_name: str, project_path: str, prompt_path: str) -> str:
-    try:
-        model = Model(model_name)
-    except ValueError as e:
-        raise ModelNameError("An error had occurred:") from e
+    model = Model(model_name)
     prompt = get_prompt(prompt_path, project_path)
     response = model.ask(prompt)
     return response
