3f705ec32c10c656c7f48c3c647d8783fc28808e
Oldranda1414 <leonardo.randacio@gmail.com>
2025-07-17 18:41:30 +0200

    feat: install litellm package

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from litellm import completion
+
 from errors import ModelNameError
 
 class Model:
@@ -5,11 +7,20 @@ class Model:
         if not _is_valid_name(name):
             raise ModelNameError(f"Model {{name}} is not one of the accepted model names")
         # TODO implement this model initialization (using litellm)
+        print(_testing())
 
     def ask(self, message: str) -> str:
         # TODO implement this
-        print("model was asked: \n" + message)
+        # print("model was asked: \n" + message)
         return "model was asked: " + message
+
+def _testing():
+    response = completion(
+                model="ollama/llama2",
+                messages = [{ "content": "Hello, how are you?","role": "user"}],
+                api_base="http://localhost:11434"
+    )
+    return response
 
     
 def _is_valid_name(model_name):
